ffb0555ad3935b18f54d36b201ea3eae70654427
Ciputra-Md <115344815+Ciputra-Md@users.noreply.github.com>
2023-06-07 13:40:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,29 +26,31 @@ global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = english //change indonesia to english if you don't understand the language used by the bot
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
-global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
-global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
-global.region = "Buddhist" //bebas
-global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.botname = "Grup Menginap Opung Bot-MD" //namabot kalian
+global.ownername = "Grup Menginap Opung" //nama kalian
+global.myweb = "https://youtube.com/@GrupMenginapOpung" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@GrupMenginapOpung" //bebas asal jan hapus
+global.myweb2 = "https://youtube.com/@GrupMenginapOpung" //bebas
+global.email = "opungmenginapopung@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.timezone = 'Asia/Jakarta' //  timezone wib
+global.timezone = 'Asia/Denpasar' //  timezone wita
+global.timezone = 'Asia/Ambon' //  timezone wit
+global.premium = ['895365575377'] //premium user
+global.owner = ["895365575377", "0895365544061"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "895365575377" // nomor wa kalian
+global.ownernomerr = "+895365544061" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Md' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '©GrupMenginapOpung' //sticker wm ubah
+global.author = 'Di Buat Oleh Apple Televisi Indonesia Media //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
